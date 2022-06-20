7a4f5f640375e5c3bc75df626694f9df0d756bfb
dfinke <finked@hotmail.com>
2022-06-20 17:10:52 -0400

    add LayoutEngine

--- psgraphNB.ps1
+++ psgraphNB.ps1
@@ -1,8 +1,8 @@
 using namespace "Microsoft.DotNet.Interactive"
 
 function Write-Notebook {
-    # via James O'Neill, in https://github.com/dfinke/PowerShellPivot
-    <#
+  # via James O'Neill, in https://github.com/dfinke/PowerShellPivot
+  <#
         .SYNOPSIS
           Writes to  the output part of the current cell (a streamlined version of Out-Display)
   
@@ -27,55 +27,72 @@ function Write-Notebook {
   
           Converts $psversionTable to a table and displays it. Without Write-Notebook the HTML markup would appear.
       #>
-    [cmdletbinding(DefaultParameterSetName = 'Html')]
-    param   (
-      [parameter(Mandatory = $true, ParameterSetName = 'Html', ValueFromPipeline = $true, Position = 1 )]
-      $Html,
+  [cmdletbinding(DefaultParameterSetName = 'Html')]
+  param   (
+    [parameter(Mandatory = $true, ParameterSetName = 'Html', ValueFromPipeline = $true, Position = 1 )]
+    $Html,
   
-      [parameter(Mandatory = $true, ParameterSetName = 'Text')]
-      $Text,
+    [parameter(Mandatory = $true, ParameterSetName = 'Text')]
+    $Text,
   
-      [Alias('PT')]
-      [switch]$PassThru
-    )
-    begin { $htmlbody = @() }
-    process { if ($html) { $htmlbody += $Html } }
-    end {
-      if ($htmlbody.count -gt 0) { $result = [Kernel]::display([Kernel]::HTML($htmlbody), 'text/html') }
-      if ($Text) { $result = [Kernel]::display($Text, 'text/plain') }
-      if ($PassThru) { return $result }
-    }
+    [Alias('PT')]
+    [switch]$PassThru
+  )
+  begin { $htmlbody = @() }
+  process { if ($html) { $htmlbody += $Html } }
+  end {
+    if ($htmlbody.count -gt 0) { $result = [Kernel]::display([Kernel]::HTML($htmlbody), 'text/html') }
+    if ($Text) { $result = [Kernel]::display($Text, 'text/plain') }
+    if ($PassThru) { return $result }
   }
+}
   
 function Show-InNotebook {
-    param(
-        $DestinationPath = ".\test.png",
-        [Parameter(ValueFromPipeline)]
-        $graph,
-        [Switch]$KeepPNGFile
-    )
+  param(
+    $DestinationPath = ".\test.png",
+    [Parameter(ValueFromPipeline)]
+    $graph,
+    # The layout engine used to generate the image
+    [ValidateSet(
+      'Hierarchical',
+      'SpringModelSmall' ,
+      'SpringModelMedium',
+      'SpringModelLarge',
+      'Radial',
+      'Circular',
+      'dot',
+      'neato',
+      'fdp',
+      'sfdp',
+      'twopi',
+      'circo'
+    )]    
+    $LayoutEngine = 'dot',
+      
+    [Switch]$KeepPNGFile
+  )
 
-    Begin {
-        $data = @()
-    }
+  Begin {
+    $data = @()
+  }
 
-    Process {
-        $data += $graph
-    }
+  Process {
+    $data += $graph
+  }
    
-    End {
+  End {
 
-        Remove-Item $destinationPath -ErrorAction SilentlyContinue
-        $null = $data | Export-PSGraph -DestinationPath $DestinationPath
+    Remove-Item $destinationPath -ErrorAction SilentlyContinue
+    $null = $data | Export-PSGraph -DestinationPath $DestinationPath -LayoutEngine $LayoutEngine 
 
-        $png = [System.IO.File]::ReadAllBytes($DestinationPath)
-        $b64 = [System.Convert]::ToBase64String($png)
+    $png = [System.IO.File]::ReadAllBytes($DestinationPath)
+    $b64 = [System.Convert]::ToBase64String($png)
         
-        $img = '<img src="data:image/png; base64, {0}"></img>' -f $b64
+    $img = '<img src="data:image/png; base64, {0}"></img>' -f $b64
 
-        Write-Notebook -Html $img
-        if(!$KeepPNGFile) {
-            Remove-Item $destinationPath -ErrorAction SilentlyContinue
-        }
+    Write-Notebook -Html $img
+    if (!$KeepPNGFile) {
+      Remove-Item $destinationPath -ErrorAction SilentlyContinue
     }
+  }
 }
